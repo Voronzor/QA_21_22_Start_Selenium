df72beab2c400b97415c24ba617c47de89683b38
Kirill Voronkov <vorronkovkirill@gmail.com>
2023-11-26 20:53:50 +0200

    add testlogin

---
+++ src/test/java/StartSelenium.java
@@ -0,0 +1,23 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class StartSelenium {
+
+    WebDriver wd;
+
+    @Test
+    public void test(){
+        wd = new ChromeDriver();
+        //wd.get("https://telranedu.web.app/home");
+        wd.navigate().to("https://telranedu.web.app/home");
+        wd.navigate().back();
+        wd.navigate().forward();
+        wd.navigate().refresh();
+        wd.close();//only one tab if only one exist so close browser too
+        wd.quit();//all tabs and browser
+    }
+
+
+
+}
