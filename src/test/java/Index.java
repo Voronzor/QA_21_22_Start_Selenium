2fcfbf360b2839b5d3f714eb1702587a62fd2dd1
Kirill Voronkov <vorronkovkirill@gmail.com>
2023-11-29 20:59:49 +0200

    yea

---
+++ src/test/java/Index.java
@@ -0,0 +1,55 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class Index {
+    WebDriver wd;
+
+    @BeforeClass
+    public void setUp(){
+        wd = new ChromeDriver();
+        wd.get("file:///C:/Users/Voronzor/Downloads/21.index.html");
+    }
+
+    @Test
+    public void cssLocators(){
+        //by tag name
+        WebElement e1 = wd.findElement(By.tagName("button"));
+        WebElement element = wd.findElement(By.cssSelector("Button"));
+
+        List<WebElement> list = wd.findElements(By.tagName("a"));
+        int i = list.size();
+        WebElement el3 = list.get(2);
+
+
+        //by class
+        WebElement divcontainer = wd.findElement(By.className("container"));
+        WebElement divcontainer1 = wd.findElement(By.cssSelector(".container"));
+
+        List<WebElement> navlist = wd.findElements(By.className("nav-item"));
+        List<WebElement> navlist1 = wd.findElements(By.cssSelector(".nav-item"));
+
+
+        //by ID
+
+        WebElement nav = wd.findElement(By.id("nav"));
+        WebElement nav1 = wd.findElement(By.cssSelector("#nav"));
+
+        //by attribute
+
+        WebElement inputEmail = wd.findElement(By.cssSelector("[placeholder='Type your name']"));
+        WebElement a2 = wd.findElement(By.cssSelector("[href='#item2']"));
+
+        WebElement input1 = wd.findElement(By.cssSelector("[name='name']"));
+        WebElement inputS = wd.findElement(By.name("name"));
+
+
+    }
+
+
+}
